3b1c9a4149a148681dbf75c1c636f6451c10e33d
JackHyde <jack.hyde@school.barren.kyschools.us>
2023-01-17 08:33:30 -0600

    Initial Commit

---
+++ swiftVariablesExercise.playground/Contents.swift
@@ -0,0 +1,23 @@
+print ("Jack Hyde")
+
+// integers, strings, Boolean, float, double
+
+// A variable that will not change will use the LET command
+
+var name = "Chris"
+print(name)
+name = "Robert"
+print(name)
+
+// BONUS
+var number = 3
+
+number = 3+2
+
+number = 3+2+5
+
+print(number)
+
+var nums = (1, 8, 9)
+
+print(nums)
